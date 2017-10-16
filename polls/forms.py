365f59881689abf41693d28954355b66c0b93a52
thisistawm <thisistawm@gmail.com>
2017-10-15 17:22:52 -0700

    poll work

---
+++ polls/forms.py
@@ -0,0 +1,8 @@
+from django.contrib.auth.models import User
+from django import forms
+from .models import Question, Choice
+
+#class PollForm(forms.ModelForm):
+#	class Meta:
+#		model = Choice
+#		fields = ('choice_text')
