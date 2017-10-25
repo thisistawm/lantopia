a0042d46c505b9df4a49b3d82624c9420e56c07e
Darcy <darcival@gmail.com>
2017-10-24 21:22:19 -0700

    Added tab nav and basic profile styles

--- core/forms.py
+++ core/forms.py
@@ -4,20 +4,26 @@ from .models import Profile, Todo
 from django.forms.widgets import CheckboxInput
 
 class ProfileForm(forms.ModelForm):
-	class Meta:
-		model = Profile
-		fields = ('game', 'song', 'movie', 'tv')
+    class Meta:
+        model = Profile
+        fields = ('game', 'song', 'movie', 'tv')
+        widgets = {
+            'game': forms.TextInput(attrs={'class':'form-control'}),
+            'song': forms.TextInput(attrs={'class':'form-control'}),
+            'movie': forms.TextInput(attrs={'class':'form-control'}),
+            'tv': forms.TextInput(attrs={'class':'form-control'}),
+        }
 
 class TodoForm(forms.ModelForm):
     class Meta:
         model = Todo
         fields = ('sayHi', 'getGames', 'checkDrinks', 'makePicks', 'doRevel')
         widgets = {
-            'sayHi': forms.CheckboxInput(attrs={'name': 'todoUpdateButton', 'onclick':'this.form.submit();'}),
-            'getGames': forms.CheckboxInput(attrs={'name': 'todoUpdateButton', 'onclick':'this.form.submit();'}),
-            'checkDrinks': forms.CheckboxInput(attrs={'name': 'todoUpdateButton', 'onclick':'this.form.submit();'}),
-            'makePicks': forms.CheckboxInput(attrs={'name': 'todoUpdateButton', 'onclick':'this.form.submit();'}),
-            'doRevel': forms.CheckboxInput(attrs={'name': 'todoUpdateButton', 'onclick':'this.form.submit();'}),
+            'sayHi': forms.CheckboxInput(attrs={'name': 'todoUpdateButton', 'class':'form-check-input', 'onclick':'this.form.submit();'}),
+            'getGames': forms.CheckboxInput(attrs={'name': 'todoUpdateButton', 'class':'form-check-input', 'onclick':'this.form.submit();'}),
+            'checkDrinks': forms.CheckboxInput(attrs={'name': 'todoUpdateButton', 'class':'form-check-input', 'onclick':'this.form.submit();'}),
+            'makePicks': forms.CheckboxInput(attrs={'name': 'todoUpdateButton', 'class':'form-check-input', 'onclick':'this.form.submit();'}),
+            'doRevel': forms.CheckboxInput(attrs={'name': 'todoUpdateButton', 'class':'form-check-input', 'onclick':'this.form.submit();'}),
         }
         labels = {
             "sayHi":"Say hi to everyone",
